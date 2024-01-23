a23cdae12bbde144cddab8f662ca995bdb58233e
여경현 <121675605+yeokyunghyun@users.noreply.github.com>
2024-01-23 17:33:06 +0900

    _016_버블_소트_프로그램_2
    백준 1377번 버블소트 문제 통과
    
    HashMap자료구조를 이용해서 푸는 방식에서  Node객체를 사용해서 푸는 방식으로 다르게 풀어봄

--- 자료구조/_016_버블_소트_프로그램1.java
+++ 자료구조/_016_버블_소트_프로그램1.java
@@ -14,28 +14,30 @@ public class _016_버블_소트_프로그램1 {
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int count = 0;
+        int maxLength = 0;
 
-        int arr[] = new int[N];
+        Node arr[] = new Node[N];
         for(int i = 0; i < N; ++i) {
-            arr[i] = Integer.parseInt(br.readLine());
+            arr[i] = new Node(i, Integer.parseInt(br.readLine()));
         }
 
-        int result[] = arr.clone();
-        Arrays.sort(result);
-
-        HashMap<Integer, Integer> hashMap = new HashMap<>();
-        for (int i = 0; i < result.length; i++) {
-            hashMap.put(result[i], i);
-        }
-
-        int max = 0;
+        Arrays.sort(arr, (o1, o2) -> o1.value - o2.value);
 
         for(int i = 0; i < N; ++i) {
-            int dif = i - hashMap.get(arr[i]);
-            max = dif > max ? dif : max;
+            int changeIdx = arr[i].index;
+            maxLength = changeIdx - i > maxLength ? changeIdx - i : maxLength;
         }
 
-        System.out.println(max + 1);
+        System.out.println(maxLength + 1);
+    }
+
+    private static class Node {
+        int index;
+        int value;
+
+        public Node(int index, int value) {
+            this.index = index;
+            this.value = value;
+        }
     }
 }
