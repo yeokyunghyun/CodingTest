9a86a74d564a4bb44b0a8458f31e0ce738d53df9
여경현 <121675605+yeokyunghyun@users.noreply.github.com>
2024-01-23 18:22:03 +0900

    _018_ATM_인출_시간_계산하기
    백준 11399번 문제 통과
    
    삽입 정렬과 부분 합 개념을 이용해서 구현

---
+++ practice/Practice.java
@@ -0,0 +1,9 @@
+package practice;
+
+public class Practice {
+    public static void main(String[] args) {
+        String str = "Asd";
+        String result = str.substring(1,2);
+        System.out.println(result);
+    }
+}
