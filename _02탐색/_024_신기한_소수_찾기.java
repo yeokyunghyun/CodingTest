8edce70366bbc1ec25b36582e43b6dabc685f61f
여경현 <121675605+yeokyunghyun@users.noreply.github.com>
2024-01-28 17:54:13 +0900

    _024_신기한_소수_찾기
    백준 2023번 문제 통과
    
    에라토스테네스의 체 방식이 아닌 하나하나 소수인지를 검사하는 메소드를 구현해서 통과

--- _02탐색/_024_신기한_소수_찾기.java
+++ _02탐색/_024_신기한_소수_찾기.java
@@ -12,35 +12,13 @@ public class _024_신기한_소수_찾기 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
 
-        int digit = 1;
-        for (int i = 1; i <= N; ++i) {
-            digit *= 10;
-        }
-
-        boolean[] primeNum = new boolean[digit];
-
-        getPrimeNum(digit, primeNum);
-        /*false인게 primeNum*/
-
-        for (int i = 1; i <= 9; ++i) {
-            if (primeNum[i]) continue;
-            dfs(primeNum, i, 1);
-        }
+        dfs(2, 1);
+        dfs(3, 1);
+        dfs(5, 1);
+        dfs(7, 1);
     }
 
-    private static void getPrimeNum(int digit, boolean[] primeNum) {
-        primeNum[0] = true;
-        primeNum[1] = true;
-
-        for (int i = 2; i <= Math.sqrt(digit); ++i) {
-            if (primeNum[i]) continue;
-            for (int j = i + i; j < digit; j += i) {
-                primeNum[j] = true;
-            }
-        }
-    }
-
-    private static void dfs(boolean primeNum[], int num, int digit) {
+    private static void dfs(int num, int digit) {
         /*digit이 N이면 출력하고 return*/
         if (digit == N) {
             System.out.println(num);
@@ -49,18 +27,24 @@ public class _024_신기한_소수_찾기 {
         /*1, 3, 7, 9 돌리면서 덧붙인게 primeNum이면 다음껄로 dfs 재귀*/
         num *= 10;
 
-        if (!primeNum[num + 1]) {
-            dfs(primeNum, num + 1, digit + 1);
+        if (isPrime(num + 1)) {
+            dfs(num + 1, digit + 1);
         }
-        if (!primeNum[num + 3]) {
-            dfs(primeNum, num + 3, digit + 1);
+        if (isPrime(num + 3)) {
+            dfs(num + 3, digit + 1);
         }
-        if (!primeNum[num + 7]) {
-            dfs(primeNum, num + 7, digit + 1);
+        if (isPrime(num + 7)) {
+            dfs(num + 7, digit + 1);
         }
-        if (!primeNum[num + 9]) {
-            dfs(primeNum, num + 9, digit + 1);
+        if (isPrime(num + 9)) {
+            dfs(num + 9, digit + 1);
         }
+    }
 
+    public static boolean isPrime(int num) {
+        for(int i = 2; i <= Math.sqrt(num); ++i) {
+            if(num % i == 0) return false;
+        }
+        return true;
     }
 }
