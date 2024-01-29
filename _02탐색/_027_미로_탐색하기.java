909e751582e585fb5bb01e29be5195a3b067f948
여경현 <121675605+yeokyunghyun@users.noreply.github.com>
2024-01-29 18:31:34 +0900

    _027_미로_탐색하기
    백준 2178번 문제 리팩토링
    
    동서남북 방향을 int형 2차원 배열 dir을 이용해서 바꿨고 기존에 Node에서 사용하던 count를 없애고 maze를 int형 2차원배열로 바꿔서 그 전에 숫자를 추가하는 형식으로 수정함

--- _02탐색/_027_미로_탐색하기.java
+++ _02탐색/_027_미로_탐색하기.java
@@ -13,18 +13,20 @@ public class _027_미로_탐색하기 {
 
     private static int N;
     private static int M;
+    private static int dir[][] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        boolean maze[][] = new boolean[N][M];
+        int maze[][] = new int[N][M];
 
         for (int i = 0; i < N; i++) {
             char[] row = br.readLine().toCharArray();
             for (int j = 0; j < row.length; j++) {
-                if(row[j] == '1') maze[i][j] = true;
+                if(row[j] == '1') maze[i][j] = 1;
             }
         }
 
@@ -33,10 +35,10 @@ public class _027_미로_탐색하기 {
         System.out.println(result);
     }
 
-    private static int bfs(boolean maze[][], boolean visited[][]) {
+    private static int bfs(int maze[][], boolean visited[][]) {
         /*처음 타겟 Node로 초기화*/
         Queue<Node> queue = new LinkedList<>();
-        Node node = new Node(0, 0, 1);
+        Node node = new Node(0, 0);
         visited[0][0] = true;
         queue.add(node);
         /*동서남북 탐색 하면서 visited해줌 + 하면서 [N - 1][M - 1] 나오면 return 해줌*/
@@ -44,37 +46,20 @@ public class _027_미로_탐색하기 {
             Node pollNode = queue.poll();
             int y = pollNode.y;
             int x = pollNode.x;
-            int count = pollNode.count;
 
             if(y == N - 1 && x == M - 1) {
-                return count;
+                return maze[N - 1][M - 1];
             }
-            /*동*/
-            if(x + 1 >= 0 && x + 1 < M) {
-                if(maze[y][x + 1] && !visited[y][x + 1]) {
-                    queue.add(new Node(y, x + 1, count + 1));
-                    visited[y][x + 1] = true;
-                }
-            }
-            /*남*/
-            if(y + 1 >= 0 && y + 1 < N) {
-                if(maze[y + 1][x] && !visited[y + 1][x]) {
-                    queue.add(new Node(y + 1, x, count + 1));
-                    visited[y + 1][x] = true;
-                }
-            }
-            /*서*/
-            if(x - 1 >= 0 && x - 1 < M) {
-                if(maze[y][x - 1] && !visited[y][x - 1]) {
-                    queue.add(new Node(y, x - 1, count + 1));
-                    visited[y][x - 1] = true;
-                }
-            }
-            /*북*/
-            if(y - 1 >= 0 && y - 1 < N) {
-                if(maze[y - 1][x] && !visited[y - 1][x]) {
-                    queue.add(new Node(y - 1, x, count + 1));
-                    visited[y - 1][x] = true;
+
+            for(int i = 0; i < 4; ++i) {
+                int nextY = y + dir[i][0];
+                int nextX = x + dir[i][1];
+                if(nextY >= 0 && nextY < N && nextX >= 0 && nextX < M) {
+                    if(maze[nextY][nextX] != 0 && !visited[nextY][nextX]) {
+                        queue.add(new Node(nextY, nextX));
+                        maze[nextY][nextX] = maze[y][x] + 1;
+                        visited[nextY][nextX] = true;
+                    }
                 }
             }
         }
@@ -84,12 +69,10 @@ public class _027_미로_탐색하기 {
     private static class Node {
         int y;
         int x;
-        int count;
 
-        public Node(int y, int x, int count) {
+        public Node(int y, int x) {
             this.y = y;
             this.x = x;
-            this.count = count;
         }
     }
 }
